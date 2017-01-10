e05a5072389a79f768f4377622699701422ba454
Wander Capote <wanderkpot@gmail.com>
2017-01-10 10:11:13 -0200

    initial changes

---
+++ app/login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel='shortcut icon' type='image/x-icon' href='../img/logo.png' />
+
+    <title>Vsion Partners - LogIn</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <link href="../css/app/login.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <form class="form-login  col-lg-8 col-lg-offset-2">
+                    <img class="form-title img-responsive" src="../img/logo_site_branco.png"></img>
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-user icon-login" aria-hidden="true"></i></span>
+                        <input type="text" class="form-control" placeholder="Insira seu e-mail" aria-describedby="sizing-addon1">
+                    </div>   
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-lock icon-login" aria-hidden="true"></i></span>
+                        <input type="password" class="form-control" placeholder="Senha" aria-describedby="sizing-addon1">
+                    </div> 
+                    <div class="login-buttons">
+                        <button type="cancel" class="btn btn-danger col-lg-3 col-xs-3 ">Limpar</button> 
+                        <button type="submit" class="btn btn-success col-lg-9 col-xs-9 ">Entrar</button> 
+                        <button type="button" class="btn btn-primary col-lg-12 col-xs-12 btn-sm">Esqueci a senha</button>                     
+                    </div>
+                </form>          
+            </div>
+        </div>
+    </div>
+        <!-- Footer -->
+    <div class="footer">
+        Copyright &copy; Vision Partners 2017
+    </div>
+</body>
